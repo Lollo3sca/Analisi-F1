210767cbb988c103c23d84b696ea823ba7510766
Lorenzo Tresca <lorenzo.tresca10@gmail.com>
2024-09-14 23:53:37 +0200

    Aggiungo la cartella da Jupyter

---
+++ pos_converter.py
@@ -0,0 +1,22 @@
+
+def points_sistem(position):
+    
+    if position == 1:
+        points = 10
+    elif position == 2:
+        points = 8
+    elif position == 3:
+        points = 6
+    elif position == 4:
+        points = 5
+    elif position == 5:
+        points = 4
+    elif position == 6:
+        points = 3
+    elif position == 7:
+        points = 2
+    elif position == 8:
+        points = 1
+    else:
+        points = 0
+    return points
